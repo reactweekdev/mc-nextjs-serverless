5cfdf083a43eca3448bca5836b992d6d5322ac28
Marko Arsic <marsicdev@Markos-MacBook-Air.local>
2021-11-04 10:21:11 +0100

    Add project configuration

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+// @ts-check
+import PropTypes from 'prop-types'
+
+const Title = ({ text = '' }) => {
+    return <h2>{text}</h2>
+}
+
+Title.propTypes = {
+    text: PropTypes.string.isRequired,
+}
+
+export default Title
---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+import Title from 'components/Title'
+
+const About = () => (
+    <div>
+        <h1>About</h1>
+        <Title text="Welcome to Next.js about page" />
+    </div>
+)
+
+export default About
